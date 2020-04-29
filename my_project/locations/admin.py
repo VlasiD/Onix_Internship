9f9e9700107fc804c4c8ee48188b069ad6deb8b0
Vladimir <vladimirsidenko11@gmail.com>
2020-04-29 20:02:27 +0300

    add old task files to gitignore

--- my_project/locations/admin.py
+++ my_project/locations/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from locations.models import Country, City, Symbol
 
-# Register your models here.
+
+class CountryAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+class CityAdmin(admin.ModelAdmin):
+    list_filter = ['country']
+
+
+# add models to admin panel
+admin.site.register(Country, CountryAdmin)
+admin.site.register(City, CityAdmin)
+admin.site.register(Symbol)
--- my_project/locations/models.py
+++ my_project/locations/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from django.conf import settings
+from django.dispatch import receiver
+from django.db.models.signals import pre_save, post_save, pre_delete, post_delete
 
-# Create your models here.
+
+class Symbol(models.Model):
+    """Response for creating Symbol table"""
+    image = models.ImageField()
+
+
+class Country(models.Model):
+    """Response for creating County table"""
+    class Meta:
+        """"Gets plural name for table"""
+        verbose_name_plural = 'Countries'
+
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True)
+    population = models.IntegerField(default=0)
+    cities_count = models.IntegerField(default=0)
+    # create connection to Symbol table
+    flag = models.OneToOneField(Symbol, on_delete=models.CASCADE)
+    users = models.ManyToManyField(settings.AUTH_USER_MODEL)
+
+    def __str__(self):
+        return self.name
+
+
+class City(models.Model):
+    """Response for creating City table"""
+    class Meta:
+        """"Gets plural name for table"""
+        verbose_name_plural = 'Cities'
+
+    name = models.CharField(max_length=100)
+    # create connection to Country table
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+
+@receiver(pre_save, sender=City)
+def city_creating_print(instance, **kwargs):
+    print("City {} will be added to the table".format(instance.name))
+
+
+@receiver(post_save, sender=City)
+def count_increase(instance, **kwargs):
+    instance.country.cities_count += 1
+    instance.country.save()
+    print(instance.country.cities_count)
+
+
+@receiver(pre_delete, sender=City)
+def count_decrease(instance, **kwargs):
+    instance.country.cities_count -= 1
+    instance.country.save()
+
+
+@receiver(post_delete, sender=City)
+def city_deleting_print(instance, **kwargs):
+    print("City {} deleted from the table".format(instance.name))
